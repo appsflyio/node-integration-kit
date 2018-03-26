acdeae6604a944e3dc62d1fc7fac66c3841c7f2e
Mohit Bhansali <mohit@appsfly.io>
2018-03-26 16:43:12 +0530

    Updated appinstance export

--- index.js
+++ index.js
@@ -1,4 +1,57 @@
-module.exports = {
-    IntegrationKit: require("./integration").IntegrationKit,
-    AppInstance: require("./AppInstance")
-};
+/**
+ * Created by mohitbhansali on 26/03/18.
+ */
+"use strict";
+
+let request = require("request");
+let IntegrationKit = require("./integration").IntegrationKit;
+
+class AppInstance {
+    constructor(config) {
+        this.config = config;
+        if(!this.config.repoUrl) {
+            this.config.repoUrl = 'https://hub.appsfly.io';
+        }
+    }
+
+    exec(module_handle, intent, payload, userID) {
+        userID = userID || "generic";
+        payload = payload || {};
+        return new Promise((resolve, reject) => {
+            let body = {
+                intent: intent,
+                data: payload
+            };
+            IntegrationKit.generateChecksum(body, this.config.secret, (err, checksum) => {
+                if(!err){
+                    request.post({
+                        url:this.config.repoUrl + "/executor/exec",
+                        method:"POST",
+                        json:checksum,
+                        headers:{
+                            "X-Module-Handle":module_handle,
+                            "X-App-Key":this.config.appKey,
+                            "X-UUID":userID
+                        }
+                    }, (err, resp, body) => {
+                        if(err){
+                            reject(err);
+                        }
+                        else{
+                            IntegrationKit.verifyChecksum(body, this.config.secret, (err, response) => {
+                                if(!err) {
+                                    resolve(response);
+                                }
+                            });
+                        }
+                    });
+                }
+                else{
+                    reject(err);
+                }
+            })
+        });
+    }
+}
+
+module.exports.AppInstance = AppInstance;
