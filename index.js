e36478791f2fb79c4feaf9ad257ac3455b179e7a
Mohit Bhansali <mohit@appsfly.io>
2018-03-26 15:31:09 +0530

    Code formatting

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 module.exports = {
-    integrationKit: require("./integration").IntegrationKit,
+    IntegrationKit: require("./integration").IntegrationKit,
     AppInstance: require("./AppInstance")
 };
